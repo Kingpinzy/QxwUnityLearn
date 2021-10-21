99b8657a4198168ca806f7e3b607537903143d6f
Sucl <296744085@qq.com>
2021-10-21 18:08:07 +0800

    21号提交

--- 2d project/Assets/Script/PlayerControl.cs
+++ 2d project/Assets/Script/PlayerControl.cs
@@ -6,16 +6,21 @@ public class PlayerControl : MonoBehaviour
 {
     public Rigidbody2D songshu;
     public float speed;
-
+    public float jumpforce;
 
     // Start is called before the first frame update
+
     void Start()
     {
 
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
+    {
+        Jump();
+    }
+    private void FixedUpdate()
     {
         Movement();
     }
@@ -23,18 +28,25 @@ public class PlayerControl : MonoBehaviour
     {
         float horizontalmove = Input.GetAxis("Horizontal");
         float faceorientation = Input.GetAxisRaw("Horizontal");
+        
         songshu.velocity = new Vector2(horizontalmove * speed * Time.deltaTime, songshu.velocity.y);
-
-        if (horizontalmove != 0)
-        {
-            songshu.velocity = new Vector2(horizontalmove * speed * Time.deltaTime, songshu.velocity.y);
-        }
+        
         if (faceorientation != 0)
         {
             transform.localScale = new Vector3(faceorientation, 1, 1);
         }
+        
 
     }
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            songshu.velocity = new Vector2(songshu.velocity.x, jumpforce * Time.deltaTime);
+
+        }
+    }
+    
 
 
 
